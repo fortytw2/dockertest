55bb8ad9c286025e17ad83ac6b1fac9de96a5a99
Ian Chiles <fortytw2@gmail.com>
2017-04-09 18:29:51 +0200

    initial commit

---
+++ container.go
@@ -0,0 +1,67 @@
+package dockertest
+
+import (
+	"fmt"
+	"net"
+	"os/exec"
+	"syscall"
+	"time"
+)
+
+// A Container is a container inside docker
+type Container struct {
+	Name string
+	Addr string
+
+	cmd *exec.Cmd
+}
+
+// Shutdown ends the container
+func (c *Container) Shutdown() {
+	c.cmd.Process.Signal(syscall.SIGINT)
+	time.Sleep(250 * time.Millisecond)
+}
+
+// RunContainer runs a given docker container and returns a port on which the
+// container can be reached
+func RunContainer(container string, port string, waitFunc func(addr string) error) (*Container, error) {
+	free := freePort()
+	addr := fmt.Sprintf("localhost:%d", free)
+	cmd := exec.Command("docker", "run", "-p", fmt.Sprintf("%d:%s", free, port), container)
+	// run this in the background
+	go func() {
+		err := cmd.Run()
+		if err != nil {
+			fmt.Printf("could not run container, %s\n", err)
+		}
+	}()
+
+	for {
+		err := waitFunc(addr)
+		if err == nil {
+			time.Sleep(time.Millisecond * 150)
+			break
+		}
+	}
+
+	return &Container{
+		Name: container,
+		Addr: addr,
+		cmd:  cmd,
+	}, nil
+}
+
+func freePort() int {
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
+	if err != nil {
+		panic(err)
+	}
+
+	l, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+
+	return l.Addr().(*net.TCPAddr).Port
+}
---
+++ container_test.go
@@ -0,0 +1,37 @@
+package dockertest
+
+import (
+	"database/sql"
+	"strings"
+	"testing"
+
+	"os/exec"
+
+	_ "github.com/lib/pq"
+)
+
+func TestRunContainer(t *testing.T) {
+	container, err := RunContainer("postgres:alpine", "5432", func(addr string) error {
+		db, err := sql.Open("postgres", "postgres://postgres:postgres@"+addr+"?sslmode=disable")
+		if err != nil {
+			return err
+		}
+
+		return db.Ping()
+	})
+	if err != nil {
+		t.Fatalf("could not start postgres, %s", err)
+	}
+
+	container.Shutdown()
+
+	buf, err := exec.Command("docker", "ps").CombinedOutput()
+	if err != nil {
+		t.Fatalf("could not docker ps, %s", err)
+	}
+
+	lines := strings.Split(strings.TrimRight(string(buf), "\n"), "\n")
+	if len(lines) != 1 {
+		t.Fatal("container is still running after shutdown", len(lines))
+	}
+}
