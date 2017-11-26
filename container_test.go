716bea6d60b36160b685d71962220145600734f7
J. Emrys Landivar <landivar@gmail.com>
2017-11-26 15:00:33 -0600

    Move checking logic in test to function for dryness

--- container_test.go
+++ container_test.go
@@ -9,18 +9,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func TestRunContainer(t *testing.T) {
-	container, err := RunContainer("postgres:alpine", "5432", func(addr string) error {
-		db, err := sql.Open("postgres", "postgres://postgres:postgres@"+addr+"?sslmode=disable")
-		if err != nil {
-			return err
-		}
-		return db.Ping()
-	})
-
-	if err != nil {
-		t.Fatalf("could not start postgres, %s", err)
-	}
+func checkDocker(t *testing.T, container *Container) int {
 	buf, err := exec.Command("docker", "ps").CombinedOutput()
 	if err != nil {
 		t.Fatalf("could not docker ps, %s", err)
@@ -33,25 +22,28 @@ func TestRunContainer(t *testing.T) {
 			count++
 		}
 	}
-	if count != 1 {
-		t.Fatal("container did not start or died early", len(lines))
+	return count
+}
+
+func TestRunContainer(t *testing.T) {
+	container, err := RunContainer("postgres:alpine", "5432", func(addr string) error {
+		db, err := sql.Open("postgres", "postgres://postgres:postgres@"+addr+"?sslmode=disable")
+		if err != nil {
+			return err
+		}
+		return db.Ping()
+	})
+
+	if err != nil {
+		t.Fatalf("could not start postgres, %s", err)
+	}
+	if count := checkDocker(t, container); count != 1 {
+		t.Fatal("container did not start or died early", count)
 	}
 
 	container.Shutdown()
 
-	buf, err = exec.Command("docker", "ps").CombinedOutput()
-	if err != nil {
-		t.Fatalf("could not docker ps, %s", err)
-	}
-
-	lines = strings.Split(strings.TrimRight(string(buf), "\n"), "\n")
-	count = 0
-	for _, line := range lines {
-		if strings.Contains(line, container.Name) {
-			count++
-		}
-	}
-	if count != 0 {
+	if count := checkDocker(t, container); count != 0 {
 		t.Fatal("container is still running after shutdown", count)
 	}
 }
